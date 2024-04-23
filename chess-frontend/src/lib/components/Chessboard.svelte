3b153e0c63cc41ac9b8d6730df5c4e5e3e361ced
mikk39k1 <mikk39k1@stud.kea.dk>
2024-04-23 20:19:45 +0200

    Added svg for pieces and color for squares

--- chess-frontend/src/lib/components/Chessboard.svelte
+++ chess-frontend/src/lib/components/Chessboard.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import {Chess} from 'chess.js';
     import type {Square} from 'chess.js';
+    import {Chess} from 'chess.js';
     import {onMount} from "svelte";
 
     let chess = new Chess();
@@ -15,39 +15,41 @@
     type SelectedSquare = { x: number; y: number } | null;
     let selectedSquare: SelectedSquare = null;
 
-    /* IMPLEMENT THIS WHEN FEN CAN BE FETCHED FROM BACKEND */
 
-    /* onMount(async () => {
-        await fetchFEN();
+    onMount(async () => {
+        // await fetchFEN();
     });
 
-    function loadFEN(fen: string) {
-        chess.load(fen);
-        board = chess.board();
-        boardArray = getBoardArray();
-        turn = chess.turn();
-    }
+    /* IMPLEMENT THIS WHEN FEN CAN BE FETCHED FROM BACKEND */
 
-    async function fetchFEN(): Promise<void> {
-        try {
-            const response = await fetch('/api/get_fen');
-            if (!response.ok) {
-                throw new Error('Failed to fetch FEN from backend');
-            }
 
-            const data = await response.json();
-            loadFEN(data.fen);
-        } catch (error) {
-            console.error("Error fetching FEN from backend:", error);
-            message = "Error fetching FEN from backend";
+    /*
+        function loadFEN(fen: string) {
+            chess.load(fen);
+            board = chess.board();
+            boardArray = getBoardArray();
+            turn = chess.turn();
         }
-    }
-     */
+
+        async function fetchFEN(): Promise<void> {
+            try {
+                const response = await fetch('/api/get_fen');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch FEN from backend');
+                }
+
+                const data = await response.json();
+                loadFEN(data.fen);
+            } catch (error) {
+                console.error("Error fetching FEN from backend:", error);
+                message = "Error fetching FEN from backend";
+            }
+        }
+         */
 
     function getBoardArray(): BoardArray {
         return board.map(row => row.map(piece => piece ? piece.type + piece.color : null));
     }
-
 
 
     function selectSquare(x: number, y: number): void {
@@ -94,23 +96,32 @@
     function toAlgebraic(x: number, y: number): Square {
         return ('abcdefgh'[x] + (8 - y)) as Square;
     }
+
 </script>
-
-
 <div class="flex flex-col w-full h-[1000px] justify-center items-center">
-    <div class="text-2xl my-10">{turn === 'w' ? 'White' : 'Black'}'s turn</div>
-    <div class="text-2xl my-10">{message}</div>
-    <div class="grid grid-cols-8 w-[500px] h-[600px]">
+    <div class="text-2xl mb-5">{turn === 'w' ? 'White' : 'Black'}'s turn</div>
+    <div class="text-2xl">{message}</div>
+    <div class="grid grid-cols-8 w-[600px] h-[600px]">
         {#each boardArray as row, y}
             {#each row as cell, x}
                 <div
                         role="button"
                         tabindex="0"
-                        class="w-12 h-12 flex justify-center items-center {((x + y) % 2 === 0) ? 'bg-white' : 'bg-gray-800'} {selectedSquare !== null && selectedSquare.x === x && selectedSquare.y === y ? 'bg-blue-500' : ''}"
+                        class=" w-20 h-20 flex justify-center items-center
+                        {((x + y) % 2 === 0) ? 'bg-[#f0dab5]' : 'bg-[#b58763]'}
+                           {selectedSquare !== null && selectedSquare.x === x && selectedSquare.y === y ?
+                            'bg-[#E2FF5A]' : ''}
+                        "
                         on:click={() => selectSquare(x, y)}
                         on:keydown={(e) => e.key === 'Enter' && selectSquare(x, y)}
                 >
-                    {cell === null ? '' : cell}
+                    {#if cell !== null}
+                        <img src="/assets/{cell}.svg"
+                             alt="piece"
+                             width="65"
+                        />
+
+                    {/if}
                 </div>
             {/each}
         {/each}
